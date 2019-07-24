ecaeb0e0145d9de1e38a79d6f75618870e856607
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-07-24 14:26:54 +0530

    Added basic application

---
+++ main.go
@@ -0,0 +1,14 @@
+package webapp
+
+func main() {
+	log.Println("Server started on: http://localhost:8080")
+	createDatabaseTable()
+    http.HandleFunc("/", Index)
+    http.HandleFunc("/show", Show)
+    http.HandleFunc("/new", New)
+    http.HandleFunc("/edit", Edit)
+    http.HandleFunc("/insert", Insert)
+    http.HandleFunc("/update", Update)
+    http.HandleFunc("/delete", Delete)
+    http.ListenAndServe(":8080", nil)
+}
---
+++ sql.go
@@ -0,0 +1,180 @@
+package webapp
+
+import (
+    "database/sql"
+	"log"
+	"os"
+    "net/http"
+    "text/template"
+	"github.com/go-sql-driver/mysql"
+)
+
+type Employee struct {
+    Id    int
+    Name  string
+    City  string
+}
+
+var tmpl = template.Must(template.New("Employee Management Template").Parse(htmltemplate))
+
+func dbConn() (db *sql.DB) {
+    dbDriver := "mysql"
+    dbUser := os.Getenv("DB_USER")
+    dbPass := os.Getenv("DB_PASSWORD")
+    dbName := "employeedb"
+    db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+    if err != nil {
+        panic(err.Error())
+    }
+    return db
+}
+
+func createDatabaseTable() {
+	db = dbConn()
+	_,err = db.Exec("CREATE DATABASE employeedb")
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		log.Println("Successfully created the database employeedb")
+	}
+
+	_,err = db.Exec("USE employeedb")
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		log.Println("Using employeedb for database")
+	}
+
+	stmt, err := db.Prepare("CREATE Table employee(id int NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, city varchar(50) NOT NULL, PRIMARY_KEY(id));")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		log.Println("Table created with the name employee")
+	}
+	defer db.Close()
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    selDB, err := db.Query("SELECT * FROM Employee ORDER BY id DESC")
+    if err != nil {
+        panic(err.Error())
+    }
+    emp := Employee{}
+    res := []Employee{}
+    for selDB.Next() {
+        var id int
+        var name, city string
+        err = selDB.Scan(&id, &name, &city)
+        if err != nil {
+            panic(err.Error())
+        }
+        emp.Id = id
+        emp.Name = name
+        emp.City = city
+        res = append(res, emp)
+    }
+    tmpl.ExecuteTemplate(w, "Index", res)
+    defer db.Close()
+}
+
+func Show(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    nId := r.URL.Query().Get("id")
+    selDB, err := db.Query("SELECT * FROM Employee WHERE id=?", nId)
+    if err != nil {
+        panic(err.Error())
+    }
+    emp := Employee{}
+    for selDB.Next() {
+        var id int
+        var name, city string
+        err = selDB.Scan(&id, &name, &city)
+        if err != nil {
+            panic(err.Error())
+        }
+        emp.Id = id
+        emp.Name = name
+        emp.City = city
+    }
+    tmpl.ExecuteTemplate(w, "Show", emp)
+    defer db.Close()
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+    tmpl.ExecuteTemplate(w, "New", nil)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    nId := r.URL.Query().Get("id")
+    selDB, err := db.Query("SELECT * FROM Employee WHERE id=?", nId)
+    if err != nil {
+        panic(err.Error())
+    }
+    emp := Employee{}
+    for selDB.Next() {
+        var id int
+        var name, city string
+        err = selDB.Scan(&id, &name, &city)
+        if err != nil {
+            panic(err.Error())
+        }
+        emp.Id = id
+        emp.Name = name
+        emp.City = city
+    }
+    tmpl.ExecuteTemplate(w, "Edit", emp)
+    defer db.Close()
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    if r.Method == "POST" {
+        name := r.FormValue("name")
+        city := r.FormValue("city")
+        insForm, err := db.Prepare("INSERT INTO Employee(name, city) VALUES(?,?)")
+        if err != nil {
+            panic(err.Error())
+        }
+        insForm.Exec(name, city)
+        log.Println("INSERT: Name: " + name + " | City: " + city)
+    }
+    defer db.Close()
+    http.Redirect(w, r, "/", 301)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    if r.Method == "POST" {
+        name := r.FormValue("name")
+        city := r.FormValue("city")
+        id := r.FormValue("uid")
+        insForm, err := db.Prepare("UPDATE Employee SET name=?, city=? WHERE id=?")
+        if err != nil {
+            panic(err.Error())
+        }
+        insForm.Exec(name, city, id)
+        log.Println("UPDATE: Name: " + name + " | City: " + city)
+    }
+    defer db.Close()
+    http.Redirect(w, r, "/", 301)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    emp := r.URL.Query().Get("id")
+    delForm, err := db.Prepare("DELETE FROM Employee WHERE id=?")
+    if err != nil {
+        panic(err.Error())
+    }
+    delForm.Exec(emp)
+    log.Println("DELETE")
+    defer db.Close()
+    http.Redirect(w, r, "/", 301)
+}
