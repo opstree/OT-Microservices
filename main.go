a61367a85de9ffa3c6cc4cf8ae0a0582aa6b2737
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-07-27 08:46:07 +0530

    Updated code

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"gitlab.com/opstree/ot-go-webapp/webapp"
 	"time"
+	"fmt"
 )
 
 func main() {
 	webapp.Run()
 	for t := range time.NewTicker(2 * time.Second).C {
+		fmt.Println(t)
 		webapp.HealthCheck()
 	}
 }
