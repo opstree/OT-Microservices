a35cd3fb8d4189136fc2728cdd5332cdd890c545
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-07-27 09:06:03 +0530

    Updated code

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"gitlab.com/opstree/ot-go-webapp/webapp"
 	"time"
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	for t := range time.NewTicker(2 * time.Second).C {
 		fmt.Println(t)
-		webapp.HealthCheck()
+		mux := webapp.HealthCheck()
+		http.ListenAndServe(":9000", mux)
 		webapp.Run()
 	}
 }
