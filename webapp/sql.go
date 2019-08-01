d1293449184d18ec4e83b09c231bfb6b278ec5b0
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-08-01 14:46:31 +0530

    Updated var

--- webapp/sql.go
+++ webapp/sql.go
@@ -80,7 +80,7 @@ func createTable() {
 		log.Info("USING employeedb database")
     }
     
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS Employee ( id int(6) NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, city varchar(50) NOT NULL, email varchar(50) NOT NULL, date DATE, PRIMARY KEY (id) )")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS Employee ( id int(6) NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, city varchar(50) NOT NULL, email varchar(50) NOT NULL, date varchar(50), PRIMARY KEY (id) )")
 	if err != nil {
 		log.Error(err.Error())
 	} else {
