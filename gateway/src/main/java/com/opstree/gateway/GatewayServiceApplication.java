5bce24788b33d9d07127dfe89cc1629086f57894
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2020-07-11 23:39:08 +0530

    Added logging for spring application

--- gateway/src/main/java/com/opstree/gateway/GatewayServiceApplication.java
+++ gateway/src/main/java/com/opstree/gateway/GatewayServiceApplication.java
@@ -4,6 +4,8 @@ package com.opstree.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import com.opstree.gateway.filters.pre.SimpleFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,5 +13,10 @@ public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
 	}
 }
