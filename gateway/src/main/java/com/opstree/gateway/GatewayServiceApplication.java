16476b9944492606d614a71d7805ce6463860ad7
Abhishek Dubey <abhishekbhardwaj510@gmail.com>
2022-09-30 13:26:51 +0530

    [Apm integration][Add] Added APM integration for Gateway application (#9)
    * Added APM logic for apm-integration
    
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
    
    * Modified codebase to support APM
    
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
    
    * Added integration for elastic APM
    
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
    
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

--- gateway/src/main/java/com/opstree/gateway/GatewayServiceApplication.java
+++ gateway/src/main/java/com/opstree/gateway/GatewayServiceApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import com.opstree.gateway.filters.pre.SimpleFilter;
+import co.elastic.apm.attach.ElasticApmAttacher;
 
 @SpringBootApplication
 @EnableZuulProxy
-public class GatewayServiceApplication {
 
+public class GatewayServiceApplication {
 	public static void main(String[] args) {
+		ElasticApmAttacher.attach();
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
